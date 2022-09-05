a92edc69e2a2f7e48ce1991400dd2245dee2dcd8
Ayberk <49751303+ayberkercan@users.noreply.github.com>
2022-09-06 01:08:52 +0300

    Update FirstTestCode.java

--- src/test/java/FirstTestCode.java
+++ src/test/java/FirstTestCode.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -6,13 +8,19 @@ public class FirstTestCode {
     @Test
     public void openWebsite(){
         WebDriver driver = new ChromeDriver();
-        System.out.println("driver sürücüsü :Chrome da browser olusturma");
+     //   System.out.println("driver sürücüsü :Chrome da browser olusturma");
         driver.manage().window().maximize();
-        System.out.println("Browser ekran boyutu ayarlama");
-        driver.navigate().to("https://www.w3schools.com/");
-        System.out.println(" Bir Web sitesine giriş yapma");
-//driver.get("https://www.w3schools.com/");
-        driver.close();
-        System.out.println("Browser kapatma");
+      //  System.out.println("Browser ekran boyutu ayarlama");
+       // driver.navigate().to("https://www.w3schools.com/");
+      //  System.out.println(" Bir Web sitesine giriş yapma");
+//driver.get("");
+       // driver.close();
+        //System.out.println("Browser kapatma");
+        driver.get("https://demoqa.com/text-box");
+
+        WebElement nameElement = driver.findElement(By.id("userName"));
+        nameElement.click();
+        nameElement.sendKeys("Ayberk");
+
     }
 }
